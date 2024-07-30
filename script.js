ee173c5415aac0ae9161a6309957b92072546fb8
iCreate1123 <ivayloa1123@gmail.com>
2024-07-30 14:39:15 +0300

    minor adjustments

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-let inputButton = document.querySelector("#inputButton"); 
-const container = document.querySelector(".container")
+const inputButton = document.querySelector("#inputButton"); 
+const container = document.querySelector(".container");
+const resetButton = document.querySelector("#resetButton");
+container.style.display = "none";
+
 function getInputAndDraw(event){ 
     event.preventDefault();
     let input = document.querySelector("#inputField").value;
     container.innerHTML = "";
-    for (let i = 0; i < input; i++) {
-        let childContainer = document.createElement("div");
-        childContainer.setAttribute("class","childContainer");
-        //width fit content
-        container.appendChild(childContainer)
-        for (let j = 0; j < input; j++) {
-            let square = document.createElement("div")
-            square.addEventListener("mouseover", () => {
-               square.style.backgroundColor = hexColorGen();
-            })
-            square.setAttribute("class","square")
-            childContainer.appendChild(square)
+    if (input > 100) {
+        container.textContent = "Amount should not exceed 100. Please enter a lower amount. :)";
+        container.style.border = "none";
+    }
+    if (input <= 100 && input != 0) {
+        container.style.display = "block"
+        for (let i = 0; i < input; i++) {
+            let childContainer = document.createElement("div");
+            childContainer.setAttribute("class","childContainer");
+            //width fit content
+            container.appendChild(childContainer)
+            for (let j = 0; j < input; j++) {
+                let square = document.createElement("div")
+                square.addEventListener("mouseover", () => {
+                   square.style.backgroundColor = hexColorGen();
+                })
+                square.setAttribute("class","square")
+                childContainer.appendChild(square)
+            }
         }
+    
     }
 }
 function hexColorGen() {
@@ -27,3 +38,11 @@ function hexColorGen() {
 
 inputButton.addEventListener("click", getInputAndDraw);
 
+function resetContent (event) {
+    event.preventDefault();
+    container.innerHTML = "";
+    container.style.display = "none";
+
+}
+
+resetButton.addEventListener("click" , resetContent);
